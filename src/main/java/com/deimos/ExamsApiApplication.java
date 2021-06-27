60d6d28cb50e9bbf278d6de7175942a3b22c488a
Deimos177 <brucevieira.2009@gmail.com>
2021-06-26 22:26:02 -0300

    Base application

---
+++ src/main/java/com/deimos/ExamsApiApplication.java
@@ -0,0 +1,13 @@
+package com.deimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsApiApplication.class, args);
+	}
+
+}
