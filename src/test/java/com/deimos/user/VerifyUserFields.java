7bbfc2dc4c49b4558728c64d8b8173905cac4172
Deimos177 <brucevieira.2009@gmail.com>
2021-06-29 11:46:36 -0300

    Test to verify if password was decrypted.

--- src/test/java/com/deimos/user/VerifyUserFields.java
+++ src/test/java/com/deimos/user/VerifyUserFields.java
@@ -13,6 +13,7 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,8 +25,10 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.deimos.dto.UserDto;
 import com.deimos.entities.Users;
 import com.deimos.repository.UserRepository;
+import com.deimos.services.UserService;
 import com.deimos.utils.DataSecurity;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -34,10 +37,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 public class VerifyUserFields {
 
-	
-
 	@Resource
 	private UserRepository userRepository;
+	
+	@Resource private UserService userService; 
 
 	@Autowired
 	private MockMvc mvc;
@@ -45,13 +48,13 @@ public class VerifyUserFields {
 	@Test
 	public void verifyIfUsernameExists() throws Exception {
 
-		Users user = new Users();
+		UserDto user = new UserDto();
 
 		user.setEmail("john.dor@test.com");
 		user.setPassword("Test@123");
 		user.setUsername("John-Doe");
 
-		userRepository.save(user);
+		userService.signUp(user);
 
 		Users userFinded = userRepository.findByUsername(user.getUsername());
 		Boolean usernameExists = userFinded == null;
@@ -67,9 +70,9 @@ public class VerifyUserFields {
 				.andReturn().getResponse();
 
 		assertEquals(400, result.getStatus());
-		assertEquals("Email inválido.",result.getContentAsString());
+		assertEquals("Email inválido.", result.getContentAsString());
 	}
-	
+
 	@Test
 	public void verifyIfPasswordValid() throws Exception {
 		MockHttpServletResponse result = mvc
@@ -78,9 +81,9 @@ public class VerifyUserFields {
 				.andReturn().getResponse();
 
 		assertEquals(400, result.getStatus());
-		assertEquals("Senha inválida, favor verificar padrão.",result.getContentAsString());
+		assertEquals("Senha inválida, favor verificar padrão.", result.getContentAsString());
 	}
-	
+
 	@Test
 	public void verifyIfPasswordIsEncripted() throws Exception {
 		MockHttpServletResponse result = mvc
@@ -89,7 +92,7 @@ public class VerifyUserFields {
 				.andReturn().getResponse();
 
 		assertEquals(400, result.getStatus());
-		assertEquals("Senha inválida, favor verificar padrão.",result.getContentAsString());
+		assertEquals("Senha inválida, favor verificar padrão.", result.getContentAsString());
 	}
 
 	public static String asJsonString(final Object obj) {
@@ -99,7 +102,7 @@ public class VerifyUserFields {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	@Test
 	public void givenString_whenEncrypt_thenSuccess() throws NoSuchAlgorithmException, IllegalBlockSizeException,
 			InvalidKeyException, BadPaddingException, InvalidAlgorithmParameterException, NoSuchPaddingException {
@@ -110,5 +113,22 @@ public class VerifyUserFields {
 		String cipherText = DataSecurity.encrypt(input, key, ivParameterSpec);
 		String plainText = DataSecurity.decrypt(cipherText, key, ivParameterSpec);
 		assertEquals(input, plainText);
+	}
+
+	@Test
+	public void ifPasswordWasDecrypted() throws Exception {
+		UserDto user = new UserDto();
+
+		user.setEmail("john.dor@test.com");
+		user.setPassword("Test@123");
+		user.setUsername("John-Doe");
+
+		userService.signUp(user);
+		
+		Users userRetrived = userRepository.findByUsername("John-Doe");
+		
+		String decryptedPassword = DataSecurity.decrypt(userRetrived.getPassword(), new SecretKeySpec(userRetrived.getKey(), 0, userRetrived.getKey().length, "AES"), new IvParameterSpec(userRetrived.getIv()));
+		
+		assertEquals("Test@123", decryptedPassword);
 	}
 }
