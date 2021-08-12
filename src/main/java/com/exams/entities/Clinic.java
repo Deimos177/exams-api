b269b0fd658bf2cb11c2dc1515dc07cda12146a8
S3nna <matheus9senna@hotmail.com>
2021-08-12 20:06:43 -0300

    entity Clinic created

--- src/main/java/com/exams/entities/Clinic.java
+++ src/main/java/com/exams/entities/Clinic.java
@@ -1,10 +1,96 @@
 package com.exams.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "clinics")
 public class Clinic {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Double latitude;
+
+	@Column(nullable = false)
+	private Double longitude;
+
+	@Column(nullable = false)
+	private String zipCode;
+	
+	@Column(nullable = false)
+	private String addressNumber;
+
+	private String averageScore;
+
+	public Clinic() {
+	}
+	
+	
+
+	public Clinic(Long id, Double latitude, Double longitude, String zipCode, String addressNumber,
+			String averageScore) {
+		super();
+		this.id = id;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.zipCode = zipCode;
+		this.addressNumber = addressNumber;
+		this.averageScore = averageScore;
+	}
+
+
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getAddressNumber() {
+		return addressNumber;
+	}
+
+	public void setAddressNumber(String addressNumber) {
+		this.addressNumber = addressNumber;
+	}
+
+	public String getAverageScore() {
+		return averageScore;
+	}
+
+	public void setAverageScore(String averageScore) {
+		this.averageScore = averageScore;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	
+	
 }
